a13fe0ec5f916a4d8a3f195297272fa89502901d
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-31 21:10:37 -0300

    Deletar objeto

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,16 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "../../../errors/AppError";
+import ProductRepository from "../typeorm/repositories/ProductRepository";
+
+class DeleteProductService {
+    public async execute(id: string) {
+        const productRepository = getCustomRepository(ProductRepository)
+        const productExist = await productRepository.findOne(id)
+        if(!productExist){
+            throw new AppError('Produto existe', 400)
+        }
+        await productRepository.remove(productExist)
+    }
+}
+
+export default DeleteProductService
